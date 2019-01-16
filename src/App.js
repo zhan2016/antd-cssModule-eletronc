e40bbab8eaecfbd8e0b0ea9317f19f7c1e50aa50
zhan2016 <ronzhang2015@gmail.com>
2019-01-16 09:33:16 +0800

    搭建好css Modules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import {Button} from 'antd';
+import  './test.scss';
 
 class App extends Component {
   render() {
@@ -11,14 +13,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <Button styleName="Mystyle">come on</Button>
         </header>
       </div>
     );
